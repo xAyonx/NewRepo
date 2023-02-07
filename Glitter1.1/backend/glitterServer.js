aa99ec64dbe749ec2664f7420e06de3650652704
xAyonx <115979462+xAyonx@users.noreply.github.com>
2023-02-07 16:16:15 +0100

    Update glitterServer.js

--- Glitter1.1/backend/glitterServer.js
+++ Glitter1.1/backend/glitterServer.js
@@ -2,12 +2,30 @@ const fs = require("fs");
 const cors = require("cors");
 const express = require('express');
 
+//copy n past from Getting started to run it with postgres
+const { Client } = require('pg');
+const { response } = require("express");
+const client = new Client({
+    user: "postgres",
+    database: "glitter", 
+    password: "postgres",
+    port: 5432,
+    host:"locolhost",
+})
+
+await client.connect()
+
 const app = express();
 app.use(cors())
 
 const hostname = "0.0.0.0"
 const port = 4000
-const glittsFile = "./glitts.json"
+
+//const glittsFile = "./glitts.json" is commented out, because the following lines should replace this function 
+//Adding new rows expecting to get a connection with postgres
+const res = await client.query("select * from glitts");
+console.log(res.rows) 
+
 
 class Glitt {
     user;
@@ -31,12 +49,20 @@ function readGlittsFromFile() {
         return []
     }
 }
-
+// query the content via the database
 app.get("/glitts", (request, response) => {
     response.send(readGlittsFromFile().reverse());
 });
 
-app.post("/glitts", (request, response) => {
+client.query(("select * from glitts").then((err, res) => {
+    if (!response.ok) {
+        console.log(glitts)} 
+    client.end()
+    }))
+
+
+
+/*app.post("/glitts", (request, response) => {
     const glitts = readGlittsFromFile();
     const glitt = new Glitt(request.payload);
     glitts.push(glitt)
@@ -44,6 +70,21 @@ app.post("/glitts", (request, response) => {
     response.send(glitt);
 });
 
+app.listen(4000, () => {
+    console.log("Listen on the port 4000...");
+});*/
+
+//Post entry to the database
+app.post('/glitts', (req, res)=> {
+    const glitts = req.pg;
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
 app.listen(4000, () => {
     console.log("Listen on the port 4000...");
 });
